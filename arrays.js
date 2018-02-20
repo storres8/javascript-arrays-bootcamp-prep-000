c00f55cbf72dc20edbf57aeed200d14c462e95bc
Steven Torres <stevetorres7562@gmail.com>
2018-02-20 02:10:47 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,7 @@ function destructivelyRemoveElementFromEndOfArray (chocolateBars) {
   return chocolateBars; 
 } 
 
-function removeElementFromEndOfArray(chocolateBars) { chocolateBars.slice(0,chocolateBars.length - 1)
+function removeElementFromEndOfArray(chocolateBars) { chocolateBars.slice (0,chocolateBars.length-1)
 return chocolateBars } 
 
   
